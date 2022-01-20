dc49fac280bebd93310a52c595e33c1465e40f63
carvalhocaio <caio.carvalho@trustedhealth.com>
2022-01-19 21:03:11 -0400

    implement a post-save-hook

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,5 +1,8 @@
+import imp
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -10,3 +13,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# Create a Profile for each new user
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
