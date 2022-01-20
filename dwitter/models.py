d26130241c70e80001ee994e19442f06d6f29cc8
carvalhocaio <caio.carvalho@trustedhealth.com>
2022-01-20 15:54:31 -0400

    Create the Back-End Logic for Dweets

--- dwitter/models.py
+++ dwitter/models.py
@@ -23,3 +23,12 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
         user_profile.follows.set([instance.profile.id])
         user_profile.save()
+
+
+class Dweet(models.Model):
+    user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} ({self.created_at:%Y-%m-%d %H:%M}): {self.body[:30]}"
